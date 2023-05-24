f10755b219ab49567e5242cfd1d5665d5dc96ad3
Ryo Sasaki <ryo.ssk323@gmail.com>
2023-05-24 18:43:12 -0400

    Edit version

--- dist/index.cjs
+++ dist/index.cjs
@@ -23,54 +23,74 @@ __export(src_exports, {
   default: () => src_default
 });
 module.exports = __toCommonJS(src_exports);
+var DEFAULT_NAMESPACES = {
+  session: "session",
+  userSession: "userSession"
+};
 var adapter = ({
   session: sessionRedis,
-  userSession: userSessionRedis
-}) => () => ({
-  getSession: async (sessionId) => {
-    const sessionData = await sessionRedis.get(sessionId);
-    return sessionData ? JSON.parse(sessionData) : null;
-  },
-  getSessionsByUserId: async (userId) => {
-    const sessionIds = await userSessionRedis.lrange(userId, 0, -1);
-    const sessions = await Promise.all(
-      sessionIds.map(async (sessionId) => {
-        const sessionData = await sessionRedis.get(sessionId);
-        return sessionData ? JSON.parse(sessionData) : null;
-      })
-    );
-    return sessions.filter(
-      (session) => session !== null
-    );
-  },
-  setSession: async (session) => {
-    await Promise.all([
-      userSessionRedis.set(session.user_id, session.id),
-      sessionRedis.set(
-        session.id,
-        JSON.stringify(session),
-        "EX",
-        Math.floor(Number(session.idle_expires) / 1e3)
-      )
-    ]);
-  },
-  deleteSession: async (...sessionIds) => {
-    const targetSessionIds = await Promise.all(
-      sessionIds.map(async (sessionId) => sessionRedis.get(sessionId))
-    );
-    const sessions = targetSessionIds.filter((sessionId) => sessionId !== null).map((sessionId) => JSON.parse(sessionId));
-    await Promise.all([
-      ...sessionIds.map((sessionId) => sessionRedis.del(sessionId)),
-      ...sessions.map((session) => userSessionRedis.del(session.user_id))
-    ]);
-  },
-  deleteSessionsByUserId: async (userId) => {
-    const sessionIds = await userSessionRedis.lrange(userId, 0, -1);
-    await Promise.all([
-      ...sessionIds.map((sessionId) => sessionRedis.del(sessionId)),
-      userSessionRedis.del(userId)
-    ]);
-  }
-});
+  namespaces = DEFAULT_NAMESPACES
+}) => {
+  const calcSessionKey = (sessionId) => `${namespaces.session}:${sessionId}`;
+  const calcUserSessionKey = (userId) => `${namespaces.userSession}:${userId}`;
+  return () => ({
+    getSession: async (sessionId) => {
+      const sessionData = await sessionRedis.get(calcSessionKey(sessionId));
+      return sessionData ? JSON.parse(sessionData) : null;
+    },
+    getSessionsByUserId: async (userId) => {
+      const sessionIds = await sessionRedis.lrange(userId, 0, -1);
+      const sessions = await Promise.all(
+        sessionIds.map(async (sessionId) => {
+          const sessionData = await sessionRedis.get(calcSessionKey(sessionId));
+          return sessionData ? JSON.parse(sessionData) : null;
+        })
+      );
+      return sessions.filter(
+        (session) => session !== null
+      );
+    },
+    setSession: async (session) => {
+      await Promise.all([
+        sessionRedis.set(calcUserSessionKey(session.user_id), session.id),
+        sessionRedis.set(
+          calcSessionKey(session.id),
+          JSON.stringify(session),
+          "EX",
+          Math.floor(Number(session.idle_expires) / 1e3)
+        )
+      ]);
+    },
+    deleteSession: async (...sessionIds) => {
+      const targetSessionIds = await Promise.all(
+        sessionIds.map(async (sessionId) => {
+          return sessionRedis.get(calcSessionKey(sessionId));
+        })
+      );
+      const sessions = targetSessionIds.filter((sessionId) => sessionId !== null).map((sessionId) => JSON.parse(sessionId));
+      await Promise.all([
+        ...sessionIds.map((sessionId) => {
+          return sessionRedis.del(calcSessionKey(sessionId));
+        }),
+        ...sessions.map((session) => {
+          return sessionRedis.lrem(
+            calcUserSessionKey(session.user_id),
+            0,
+            session.id
+          );
+        })
+      ]);
+    },
+    deleteSessionsByUserId: async (userId) => {
+      const sessionIds = await sessionRedis.lrange(userId, 0, -1);
+      await Promise.all([
+        ...sessionIds.map((sessionId) => {
+          return sessionRedis.del(calcSessionKey(sessionId));
+        }),
+        sessionRedis.del(calcUserSessionKey(userId))
+      ]);
+    }
+  });
+};
 var src_default = adapter;
 //# sourceMappingURL=index.cjs.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,8 +2,11 @@ import { Redis } from "ioredis";
 import { AdapterFunction, SessionAdapter } from "lucia-auth";
 type Params = {
     session: Redis;
-    userSession: Redis;
+    namespaces?: {
+        session: string;
+        userSession: string;
+    };
 };
-declare const adapter: ({ session: sessionRedis, userSession: userSessionRedis, }: Params) => AdapterFunction<SessionAdapter>;
+declare const adapter: ({ session: sessionRedis, namespaces, }: Params) => AdapterFunction<SessionAdapter>;
 export default adapter;
 //# sourceMappingURL=index.d.ts.map
